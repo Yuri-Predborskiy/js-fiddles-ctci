28fd2cfaffc020d0b740c453a293e1baa15cf366
Yuri Predborski <shini1984@gmail.com>
2020-05-04 15:59:49 +0200

    Update test for Chapter 1 Problem 1.9.

--- problems/1. Arrays and strings/1.9. String Rotation/_test.js
+++ problems/1. Arrays and strings/1.9. String Rotation/_test.js
@@ -1,18 +1,24 @@
-const {startTest} = require('../../../helpers/test');
+const {assert} = require('chai');
+const {it, describe} = require('mocha');
 
-const chapter = '1. Arrays and strings';
-const problem = '1.9. String Rotation';
 const tests = [
     {input: ['waterbottle', 'bottlewater'], output: true},
     {input: ['water', 'bottle'], output: false},
     {input: ['water', 'bater'], output: false},
     {input: ['aaa', 'aaa'], output: true},
 ];
-const solutions = [
-    {
-        name: 'concat, Compare concatenated rotated strings, should contain original in the middle',
-        solver: require('./concat'),
-    },
-];
 
-startTest(chapter, problem, tests, solutions);
+describe('Chapter 1. Arrays and strings', () => {
+    describe('Problem 1.9. String Rotation', () => {
+        describe('concat, Compare concatenated rotated strings, should contain original in the middle', () => {
+            const solver = require('./concat');
+
+            for (let test of tests) {
+                it(`inputs: ${JSON.stringify(test.input)}`, () => {
+                    let result = solver(...test.input);
+                    assert.equal(result, test.output);
+                });
+            }
+        });
+    });
+});
