aaed0b5ba9bad29abafcf0ceed53794acff0f1a9
Yuri Predborski <shini1984@gmail.com>
2020-05-04 14:46:48 +0200

    Update tests for Chapter 2. Make individual tests for each problem.

--- problems/2. Linked Lists/2.1. Remove Dups/_test.js
+++ problems/2. Linked Lists/2.1. Remove Dups/_test.js
@@ -1,28 +1,39 @@
-const {startTest} = require('../../../helpers/test');
+const {assert} = require('chai');
+const {it, describe} = require('mocha');
 const {convertArrayToLinkedList, convertLinkedListToArray} = require('../../../helpers/converters');
 
-const chapter = '2. Linked Lists';
-const problem = '2.1. Remove Dups';
 const tests = [
-    {input: [convertArrayToLinkedList([1,2,3,4,2,3,4,1,5])], output: [1,2,3,4,5]},
-    {input: [convertArrayToLinkedList([1,2,3])], output: [1,2,3]},
-    {input: [convertArrayToLinkedList([1,1,1,1,1,2,1,3,1,1,1,0])], output: [1,2,3,0]},
-    {input: [convertArrayToLinkedList([])], output: []},
+    {input: [1,2,3,4,2,3,4,1,5], output: [1,2,3,4,5]},
+    {input: [1,2,3], output: [1,2,3]},
+    {input: [1,1,1,1,1,2,1,3,1,1,1,0], output: [1,2,3,0]},
+    {input: [], output: []},
 ];
-const solutions = [
-    // {
-    //     name: 'no-extra-space, Remove duplicates without using extra space',
-    //     solver: require('./no-extra-space'),
-    // },
-    // {
-    //     name: 'set, Remove duplicates using a dictionary of elements',
-    //     solver: require('./set'),
-    // },
-];
-const options = {
-    comparer: 'deepEqual',
-    // initializer: convertArrayToLinkedList, // convert input into linked list
-    serializer: convertLinkedListToArray, // convert output from linked list into array
-};
+const processInputs = convertArrayToLinkedList;
+const processResult = convertLinkedListToArray;
 
-startTest(chapter, problem, tests, solutions, options);
+describe('Chapter 2. Linked Lists', () => {
+    describe('Problem 2.1. Remove Dups', () => {
+        // describe('no-extra-space, Remove duplicate list nodes without using extra space', () => {
+        //     const solver = require('./no-extra-space');
+        //
+        //     for (let test of tests) {
+        //         it(`inputs: ${JSON.stringify(test.input)}`, () => {
+        //             let args = processInputs(test.input);
+        //             let result = processResult(solver(args));
+        //             assert.deepEqual(result, test.output);
+        //         });
+        //     }
+        // });
+        describe('set, Remove duplicates using a dictionary of elements', () => {
+            const solver = require('./set');
+
+            for (let test of tests) {
+                it(`inputs: ${JSON.stringify(test.input)}`, () => {
+                    let args = processInputs(test.input);
+                    let result = processResult(solver(args));
+                    assert.deepEqual(result, test.output);
+                });
+            }
+        });
+    });
+});
